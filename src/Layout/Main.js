06b1ec3bede2f6484256702f2eb6e363a2891a75
unknown <rokihazari@gmail.com>
2022-10-26 08:30:45 +0300

    folder and layout added here

---
+++ src/Layout/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Header/Footer/Footer';
+import Header from '../Shared/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="4">
+                    </Col>
+
+                    <Col lg="8">
+                        <Outlet></Outlet>
+                    </Col>
+                   
+                </Row>
+            </Container>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
